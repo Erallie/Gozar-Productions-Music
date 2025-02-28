4e676d22f456ec4f630887802a1da327b11ae0f4
Erallie <31375589+Erallie@users.noreply.github.com>
2025-02-28 10:50:37 -0700

    Added Button and props

--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -23,3 +23,25 @@ export type AudioPlayerProps = {
     player: HTMLAudioElement;
     removeMargins?: boolean
 }
+
+export enum TextColor {
+    White,
+    Black
+}
+
+export enum ButtonDirection {
+    Right,
+    Left
+}
+
+export type ButtonProps = {
+    link: string;
+    newTab: boolean;
+    color: TextColor;
+    invertBackground?: boolean;
+    direction?: ButtonDirection;
+    customSVG?: boolean;
+    marginTopMultiplier?: number;
+    marginBottomMultiplier?: number;
+    children: any;
+}
