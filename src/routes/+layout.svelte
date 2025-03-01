fc9e4a00f2dc09fca1153ba2d8e74590fff4ec57
Erallie <31375589+Erallie@users.noreply.github.com>
2025-02-28 17:19:06 -0700

    Adding a banner for the performances

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
+    import { TextColor } from "$lib/types/types";
     import Header from "./Header.svelte";
+    import { page } from "$app/state";
     import logo from "$lib/images/gozar-productions-logo.svg";
-
     import "../app.css";
+    import Button from "./Button.svelte";
 
-    let { children, data } = $props();
+    let isPerformancePage = $state(false);
+
+    $effect(() => {
+        isPerformancePage = page.url.pathname == "/performances";
+    });
+
+    let noticeMinimized = $state(false);
+
+    function minimizeNotice() {
+        noticeMinimized = true;
+    }
+
+    let { children } = $props();
 </script>
 
 <div class="app">
@@ -13,6 +27,42 @@
     <main>
         {@render children()}
     </main>
+
+    {#if !isPerformancePage}
+        <div id="performance-banner" class={noticeMinimized ? "minimized" : ""}>
+            <div>
+                <h2>Erika Gozar</h2>
+                has<br />
+                <h2>live performances</h2>
+                in virtual reality
+            </div>
+            <Button link="/performances" newTab={false} color={TextColor.White}
+                >Attend</Button
+            >
+            <button aria-label="Minimize" onclick={minimizeNotice}
+                ><!-- <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="100"
+                    height="100"
+                    viewBox="-20 -20 140 140"
+                >
+                    <g stroke="currentColor" stroke-width="20">
+                        <line x1="0" y1="0" x2="100" y2="100" />
+                        <line x1="100" y1="0" x2="0" y2="100" />
+                    </g>
+                </svg> --><svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="120"
+                    height="40"
+                    viewBox="-10 -20 120 40"
+                >
+                    <g stroke="currentColor" stroke-width="20">
+                        <line x1="0" y1="0" x2="100" y2="0" />
+                    </g>
+                </svg></button
+            >
+        </div>
+    {/if}
 
     <footer>
         <h1>
@@ -102,5 +152,50 @@
     }
     footer a:hover {
         text-decoration: underline;
+    }
+
+    #performance-banner {
+        color: white;
+        position: fixed;
+        display: flex;
+        bottom: 0;
+        width: 100%;
+        background-color: rgba(119, 46, 46, 0.95);
+        /* text-align: center; */
+        justify-content: center;
+        & > * {
+            text-align: right;
+            margin: auto 0px;
+            & > * {
+                display: inline;
+                /* text-transform: none; */
+            }
+        }
+        & > button {
+            color: inherit;
+            opacity: 0.7;
+            background: none;
+            margin: 0px;
+            border: none;
+            width: 2em;
+            aspect-ratio: 128 / 48;
+            height: auto;
+            min-height: 0px;
+            padding: 4px;
+            position: absolute;
+            top: 4px;
+            left: 4px;
+            transition: opacity 0.2s;
+            & > svg {
+                vertical-align: top;
+                width: 100%;
+                /* aspect-ratio: 140 / 40; */
+                height: auto;
+                /* vertical-align: center; */
+            }
+            &:hover {
+                opacity: 1;
+            }
+        }
     }
 </style>
