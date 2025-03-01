78951d0b4122547890290ae6f3af4e1268dad2a1
Erallie <31375589+Erallie@users.noreply.github.com>
2025-03-01 13:02:40 -0700

    Refined transition styles for performer banner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
     import { page } from "$app/state";
     import logo from "$lib/images/gozar-productions-logo.svg";
     import "../app.css";
+    import { onDestroy, onMount } from "svelte";
     import Button from "./Button.svelte";
 
     let isPerformancePage = $state(false);
@@ -13,18 +14,56 @@
     });
 
     let attendButton: HTMLAnchorElement | null = $state(null);
-
     let attendButtonWidth = $state(300);
-
     let noticeMinimized = $state(false);
+    let performanceBanner: HTMLDivElement | null = $state(null);
+    let widthMatching = $state(false);
+
+    let resizeObserver: ResizeObserver;
+
+    const handleResize = (entries: ResizeObserverEntry[]) => {
+        for (const entry of entries) {
+            if (entry.target === attendButton) {
+                // Get the offsetWidth of the attendButton
+                attendButtonWidth = attendButton.offsetWidth;
+            } else if (entry.target === performanceBanner) {
+                // Get the regular width of the performanceBanner
+                const performanceBannerWidth = entry.contentRect.width;
+
+                // widthMatching = performanceBannerWidth === attendButtonWidth;
+                widthMatching =
+                    Math.abs(performanceBannerWidth - attendButtonWidth) <= 1;
+                /* // Check if the performance banner width matches the attend button width
+                if (performanceBannerWidth === attendButtonWidth) {
+                    bannerClass = 'matching-width';
+                } else {
+                    bannerClass = '';
+                } */
+            }
+        }
+    };
 
     function minimizeNotice() {
         noticeMinimized = !noticeMinimized;
+        if (!noticeMinimized) {
+            widthMatching = false;
+        }
     }
 
-    $effect(() => {
-        if (noticeMinimized && attendButton) {
-            attendButtonWidth = attendButton.offsetWidth;
+    onMount(() => {
+        if (attendButton) {
+            resizeObserver = new ResizeObserver(handleResize);
+            resizeObserver.observe(attendButton);
+        }
+        if (performanceBanner) {
+            resizeObserver.observe(performanceBanner);
+        }
+    });
+
+    onDestroy(() => {
+        if (resizeObserver) {
+            resizeObserver.disconnect();
+            console.log("disconnected resize observer");
         }
     });
 
@@ -38,56 +77,47 @@
         {@render children()}
     </main>
 
-    {#if !isPerformancePage}
-        <div
-            id="performance-banner"
-            class={noticeMinimized ? "minimized" : ""}
-            style="--attend-button-width: {attendButtonWidth}px"
-        >
-            <div>
-                <h2>Erika Gozar</h2>
-                has<br />
-                <h2>live performances</h2>
-                in virtual reality
-            </div>
-            <Button
-                bind:element={attendButton}
-                link="/performances"
-                newTab={false}
-                color={TextColor.White}
-                marginTopMultiplier={0.5}
-                marginBottomMultiplier={0.5}
-                marginLeftMultiplier={0.5}
-                marginRightMultiplier={0.5}
-                >{#if !noticeMinimized}Attend{:else}View Performances{/if}</Button
-            >
-            <button aria-label="Minimize" onclick={minimizeNotice}
-                ><svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="100"
-                    height="100"
-                    viewBox="0 -10 100 100"
-                >
-                    <g stroke="currentColor" stroke-width="20">
-                        <line
-                            id="line-vertical"
-                            x1="0"
-                            y1="0"
-                            x2="100"
-                            y2="0"
-                        />
-                        <line
-                            id="line-horizontal"
-                            x1="0"
-                            y1="0"
-                            x2="100"
-                            y2="0"
-                        />
-                    </g>
-                </svg></button
-            >
+    <div
+        id="performance-banner"
+        bind:this={performanceBanner}
+        class="{noticeMinimized ? 'minimized' : ''} {widthMatching
+            ? 'width-matching'
+            : ''}"
+        style="--attend-button-width: {attendButtonWidth}px; {isPerformancePage
+            ? 'display: none'
+            : ''}"
+    >
+        <div>
+            <h2>Erika Gozar</h2>
+            has<br />
+            <h2>live performances</h2>
+            in virtual reality
         </div>
-    {/if}
+        <Button
+            bind:element={attendButton}
+            link="/performances"
+            newTab={false}
+            color={TextColor.White}
+            marginTopMultiplier={0.5}
+            marginBottomMultiplier={0.5}
+            marginLeftMultiplier={0.5}
+            marginRightMultiplier={0.5}
+            >{#if !noticeMinimized}Attend{:else}View Performances{/if}</Button
+        >
+        <button aria-label="Minimize" onclick={minimizeNotice}
+            ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="100"
+                height="100"
+                viewBox="0 -10 100 100"
+            >
+                <g stroke="currentColor" stroke-width="20">
+                    <line id="line-vertical" x1="0" y1="0" x2="100" y2="0" />
+                    <line id="line-horizontal" x1="0" y1="0" x2="100" y2="0" />
+                </g>
+            </svg></button
+        >
+    </div>
 
     <footer>
         <h1>
@@ -191,11 +221,8 @@
         max-width: 100%;
         background-color: rgba(119, 46, 46, 0.95);
         /* text-align: center; */
-        justify-content: left;
+        justify-content: center;
         padding: 10px;
-        transition:
-            max-width 1s,
-            background-color 1s;
         /* & > :nth-child(2) {
             margin: auto 0px;
             background-color: black;
@@ -229,8 +256,8 @@
         }
         & > :first-child {
             text-align: right;
-            margin: auto;
-            margin-right: 10px;
+            margin: auto 10px;
+            /* margin-right: 10px; */
             /* max-width: 100%; */
             height: min-content;
             overflow: hidden;
@@ -246,10 +273,12 @@
             transition: transform 0.2s;
         }
         &.minimized {
-            background-color: rgba(119, 46, 46, 0);
+            /* background-color: rgba(119, 46, 46, 0); */
             max-width: var(--attend-button-width);
+            border-top-right-radius: 40px;
             & > :first-child {
-                margin-right: 0px;
+                margin: auto 0px;
+                /* margin-right: 0px; */
                 /* max-width: 0%; */
             }
             /* & #line-left {
@@ -265,6 +294,15 @@
                 transform: translate(0px, 40%);
             }
             /* display: none; */
+        }
+        &:not(.width-matching) {
+            transition:
+                max-width 1s,
+                background-color 1s,
+                border-top-right-radius 1s;
+        }
+        &.width-matching {
+            transition: none;
         }
     }
 </style>
