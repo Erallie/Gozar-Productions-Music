cada864d4e46b8dd47066ff50cc96d7a5b6c66c1
Erallie <31375589+Erallie@users.noreply.github.com>
2025-03-02 09:23:09 -0700

    Adjusting styles for common issues

--- src/routes/performances/+page.svelte
+++ src/routes/performances/+page.svelte
@@ -44,201 +44,223 @@
         scrolling="no"
     ></iframe>
 </section>
-<section id="how-to-attend">
-    <h1>How to Attend</h1>
-    <ol>
-        <ListSection title="Download VRChat">
-            <p>
-                <a href="https://hello.vrchat.com/" target="_blank">VRChat</a>
-                is primarily meant to be played in VR, but it is also available on
-                Windows desktop through
-                <a
-                    href="https://store.steampowered.com/app/438100/VRChat/?utm-source=hello-home"
-                    target="_blank">Steam</a
-                >
-                or on Android via
-                <a
-                    href="https://play.google.com/store/apps/details?id=com.vrchat.mobile.playstore&hl=en&utm-source=hello-home"
-                    target="_blank">Google Play</a
-                >.
-            </p>
-            <p>
-                VRChat on iOS is currently in <a
-                    href="https://hello.vrchat.com/blog/apple-ios-beta-signup"
-                    target="_blank">Closed Beta</a
-                >. It is otherwise not available on Apple products.
-            </p>
-            <p>
-                If you have none of these devices and want a cheaper option, you
-                can purchase a <a
-                    href="https://www.meta.com/quest/quest-3s/"
-                    target="_blank">Meta Quest 3S</a
-                >
-                for <strong>300 USD</strong> or a
-                <a href="https://www.meta.com/quest/quest-3/" target="_blank"
-                    >Meta Quest 3</a
-                >
-                for <strong>500 USD</strong> and download VRChat through the
-                <a
-                    href="https://www.meta.com/experiences/vrchat/1856672347794301/"
-                    target="_blank">Meta Quest store</a
-                >
-                on your device.
-            </p>
-        </ListSection>
-        <ListSection title="Join our Community">
-            <p>
-                We have a <a
-                    href="https://discord.gg/cCCEk7BX4W"
-                    target="_blank">Discord server</a
-                >
-                and a public VRChat group (<a
-                    href="https://vrc.group/ERIKA.6930"
-                    target="_blank">Erika's Voice</a
-                >) where we send announcements about our performances.
-            </p>
-            <p>
-                Join either of them, and keep an eye on the <a
-                    href="https://discord.com/channels/1102582171207741480/1220485752677470208"
-                    target="_blank">announcements channel</a
-                >
-
-                or on the
-                <a
-                    href="https://vrchat.com/home/group/grp_61d1c3b2-c8e8-492a-92ca-1a1411e741a5/posts"
-                    target="_blank">group posts</a
-                >
-                for more information on where and when each performance takes place.
-            </p>
-        </ListSection>
-        <ListSection title="Join the Instance"
-            ><p>
-                When a performance is about to start, we will send an
-                announcement on our
-                <a href="https://discord.gg/cCCEk7BX4W" target="_blank"
-                    >Discord</a
-                >
-                and in our
-                <a href="https://vrc.group/ERIKA.6930" target="_blank"
-                    >VRChat group</a
-                >
-                with information on what group to join to attend the performance.
-            </p>
-            <p>
-                <bold
-                    >This group is usually different from our own (<a
+<section id="stuff">
+    <section id="how-to-attend">
+        <h1>How to Attend</h1>
+        <ol>
+            <ListSection title="Download VRChat">
+                <p>
+                    <a href="https://hello.vrchat.com/" target="_blank"
+                        >VRChat</a
+                    >
+                    is primarily meant to be played in VR, but it is also available
+                    on Windows desktop through
+                    <a
+                        href="https://store.steampowered.com/app/438100/VRChat/?utm-source=hello-home"
+                        target="_blank">Steam</a
+                    >
+                    or on Android via
+                    <a
+                        href="https://play.google.com/store/apps/details?id=com.vrchat.mobile.playstore&hl=en&utm-source=hello-home"
+                        target="_blank">Google Play</a
+                    >.
+                </p>
+                <p>
+                    VRChat on iOS is currently in <a
+                        href="https://hello.vrchat.com/blog/apple-ios-beta-signup"
+                        target="_blank">Closed Beta</a
+                    >. It is otherwise not available on Apple products.
+                </p>
+                <p>
+                    If you have none of these devices and want a cheaper option,
+                    you can purchase a <a
+                        href="https://www.meta.com/quest/quest-3s/"
+                        target="_blank">Meta Quest 3S</a
+                    >
+                    for <strong>300 USD</strong> or a
+                    <a
+                        href="https://www.meta.com/quest/quest-3/"
+                        target="_blank">Meta Quest 3</a
+                    >
+                    for <strong>500 USD</strong> and download VRChat through the
+                    <a
+                        href="https://www.meta.com/experiences/vrchat/1856672347794301/"
+                        target="_blank">Meta Quest store</a
+                    >
+                    on your device.
+                </p>
+            </ListSection>
+            <ListSection title="Join our Community">
+                <p>
+                    We have a <a
+                        href="https://discord.gg/cCCEk7BX4W"
+                        target="_blank">Discord server</a
+                    >
+                    and a public VRChat group (<a
                         href="https://vrc.group/ERIKA.6930"
                         target="_blank">Erika's Voice</a
-                    >) and is specific to the venue.</bold
-                >
-            </p>
-            <p>
-                Make sure you join the correct group, and then launch VRChat and
-                navigate to the <bold>Groups</bold> tab.
-            </p>
-            <p>
-                Under
-                <bold>Group Activity</bold>, you should see a group instance
-                open for the group you joined.
-            </p>
-            <p>Join that instance and enjoy the show!</p>
-        </ListSection>
-        <ListSection title="Common Issues"
-            ><ul>
-                <ListSection title="I'm Lagging">
-                    <p>
-                        Rendering 80 people in a single instance all at once can
-                        be pretty intensive. To avoid lag, keep the following in
-                        mind:
-                    </p>
-                    <ul>
-                        <ListSection title="Safety Settings">
-                            <p>
-                                Make sure you adjust your <bold
-                                    >safety settings</bold
-                                >
-                                to avoid rendering parts of avatars that can lag
-                                you.
-                            </p>
-                            <p>
-                                This setting can be found by clicking the <bold
-                                    >shield icon</bold
-                                >
-                                on the <bold>top-right</bold> of your
-                                <bold>big menu</bold>.
-                            </p>
-                            <p>
-                                If you are on PCVR, Make sure to turn <bold
-                                    >shaders off</bold
-                                > for most people, as those are the most intensive.
-                            </p>
-                        </ListSection>
-                        <ListSection title="Avatar Culling">
-                            <p>
-                                Turn on <bold>avatar culling</bold> to avoid rendering
-                                avatars past a certain distance away from you.
-                            </p>
-                            <p>
-                                This setting can be found in your <bold
-                                    >small menu</bold
-                                >
-                                under the <bold>settings tab</bold> under the
-                                header
-                                <bold>"Avatar Culling"</bold>.
-                            </p>
-                            <p>
-                                This setting is <bold>more effective</bold> than
-                                turning safety settings to maximum or turning
-                                safe mode on, as it renders everyone as
-                                <bold>non-skeletal diamonds</bold> instead of robots.
-                            </p></ListSection
-                        >
-                    </ul>
-                </ListSection>
+                    >) where we send announcements about our performances.
+                </p>
+                <p>
+                    Join either of them, and keep an eye on the <a
+                        href="https://discord.com/channels/1102582171207741480/1220485752677470208"
+                        target="_blank">announcements channel</a
+                    >
 
-                <ListSection title="I don't hear the performer">
-                    <p>
-                        Depending on the venue, the performer may be
-                        transmitting their audio either <bold
-                            >through their mic</bold
-                        >
-                        or by
-                        <bold>streaming to a video player</bold> in the world.
-                    </p>
-                    <p>
-                        If you don't hear the performance, you may need to make
-                        sure your <bold>world audio</bold> is turned up in your audio
-                        settings.
-                    </p>
-                    <p>
-                        If that still doesn't fix it, make sure you <bold
-                            >allow untrusted URLs</bold
-                        >
-                        in your settings. This can be found in your
-                        <bold>big menu</bold>
-                        under the <bold>Comfort & Safety</bold> tab, under the
-                        <bold>Safety</bold> heading.
-                    </p>
-                    <p>
-                        If you've done all of these things, but the issue still
-                        persists, there may be an issue with the video player.
-                        Try <bold>resyncing the video</bold> to update your
-                        feed. The button for this should be by the
-                        <bold>video controls</bold> in the world.
-                    </p>
-                </ListSection>
-            </ul></ListSection
-        >
-    </ol>
+                    or on the
+                    <a
+                        href="https://vrchat.com/home/group/grp_61d1c3b2-c8e8-492a-92ca-1a1411e741a5/posts"
+                        target="_blank">group posts</a
+                    >
+                    for more information on where and when each performance takes
+                    place.
+                </p>
+            </ListSection>
+            <ListSection title="Join the Instance"
+                ><p>
+                    When a performance is about to start, we will send an
+                    announcement on our
+                    <a href="https://discord.gg/cCCEk7BX4W" target="_blank"
+                        >Discord</a
+                    >
+                    and in our
+                    <a href="https://vrc.group/ERIKA.6930" target="_blank"
+                        >VRChat group</a
+                    >
+                    with information on what group to join to attend the performance.
+                </p>
+                <p>
+                    <bold
+                        >This group is usually different from our own (<a
+                            href="https://vrc.group/ERIKA.6930"
+                            target="_blank">Erika's Voice</a
+                        >) and is specific to the venue.</bold
+                    >
+                </p>
+                <p>
+                    Make sure you join the correct group, and then launch VRChat
+                    and navigate to the <bold>Groups</bold> tab.
+                </p>
+                <p>
+                    Under
+                    <bold>Group Activity</bold>, you should see a group instance
+                    open for the group you joined.
+                </p>
+                <p>Join that instance and enjoy the show!</p>
+            </ListSection>
+        </ol>
+    </section>
+    <section id="common-issues">
+        <h1>Common Issues</h1>
+        <ul>
+            <ListSection title="I'm Lagging">
+                <p>
+                    Rendering 80 people in a single instance all at once can be
+                    pretty intensive. To avoid lag, keep the following in mind:
+                </p>
+                <ul>
+                    <ListSection title="Safety Settings">
+                        <p>
+                            Make sure you adjust your <bold
+                                >safety settings</bold
+                            >
+                            to avoid rendering parts of avatars that can lag you.
+                        </p>
+                        <p>
+                            This setting can be found by clicking the <bold
+                                >shield icon</bold
+                            >
+                            on the <bold>top-right</bold> of your
+                            <bold>big menu</bold>.
+                        </p>
+                        <p>
+                            If you are on PCVR, Make sure to turn <bold
+                                >shaders off</bold
+                            > for most people, as those are the most intensive.
+                        </p>
+                    </ListSection>
+                    <ListSection title="Avatar Culling">
+                        <p>
+                            Turn on <bold>avatar culling</bold> to avoid rendering
+                            avatars past a certain distance away from you.
+                        </p>
+                        <p>
+                            This setting can be found in your <bold
+                                >small menu</bold
+                            >
+                            under the <bold>settings tab</bold> under the header
+                            <bold>"Avatar Culling"</bold>.
+                        </p>
+                        <p>
+                            This setting is <bold>more effective</bold> than
+                            turning safety settings to maximum or turning safe
+                            mode on, as it renders everyone as
+                            <bold>non-skeletal diamonds</bold> instead of robots.
+                        </p></ListSection
+                    >
+                </ul>
+            </ListSection>
+
+            <ListSection title="I don't hear the performer">
+                <p>
+                    Depending on the venue, the performer may be transmitting
+                    their audio either <bold>through their mic</bold>
+                    or by
+                    <bold>streaming to a video player</bold> in the world.
+                </p>
+                <p>
+                    If you don't hear the performance, you may need to make sure
+                    your <bold>world audio</bold> is turned up in your audio settings.
+                </p>
+                <p>
+                    If that still doesn't fix it, make sure you <bold
+                        >allow untrusted URLs</bold
+                    >
+                    in your settings. This can be found in your
+                    <bold>big menu</bold>
+                    under the <bold>Comfort & Safety</bold> tab, under the
+                    <bold>Safety</bold> heading.
+                </p>
+                <p>
+                    If you've done all of these things, but the issue still
+                    persists, there may be an issue with the video player. Try <bold
+                        >resyncing the video</bold
+                    >
+                    to update your feed. The button for this should be by the
+                    <bold>video controls</bold> in the world.
+                </p>
+            </ListSection>
+        </ul>
+    </section>
 </section>
 
 <style>
-    #how-to-attend {
-        width: 660px;
-        & > * {
-            max-width: 90%;
-            margin-left: auto;
-            margin-right: auto;
+    #stuff {
+        display: flex;
+        width: min-content;
+        justify-content: space-between;
+        & > section {
+            max-width: 10%;
+            text-wrap-mode: nowrap;
+            background-color: rgba(0, 0, 0, 0.1);
+            /* width: 660px; */
+            overflow-x: clip;
+            transition: max-width 1s;
+            margin: 0px;
+            &:hover {
+                max-width: calc(90% - 20px);
+            }
+            & p {
+                text-wrap-mode: wrap;
+            }
+            & > * {
+                max-width: 90%;
+                margin-left: auto;
+                margin-right: auto;
+            }
+            & * {
+                overflow-x: clip;
+            }
         }
     }
 
